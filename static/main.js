bf90fd67487b8dbff4d4466f2c9e694f78528b2a
Aidenjn <nelsonai@oregonstate.edu>
2018-04-07 16:44:40 -0700

    Added some javascript

--- static/main.js
+++ static/main.js
@@ -1,13 +1,3 @@
-//function loadDoc() {
-//  var xhttp = new XMLHttpRequest();
-//  xhttp.onreadystatechange = function() {
-//    if (this.readyState == 4 && this.status == 200) {
-//     document.getElementById("demo").innerHTML = this.responseText;
-//    }
-//  };
-//  xhttp.open("GET", "goodSample.txt", true);
-//  xhttp.send();
-//}
 var titleBlock = document.getElementById('titleBlock');
 var searchButton = document.getElementById('searchButton');
 
@@ -19,9 +9,8 @@ var searchButton = document.getElementById('searchButton');
 
 
 function changeColor() {
-    titleBlock.style.backgroundColor = "red";
-    titleBlock.style.color = "red";
-    alert("hey");
+    //titleBlock.style.backgroundColor = "red";
+    //alert("hey");
 //    modalBackdrop.style.display = 'block';
 //    twitModal.style.display = 'block';
 };
