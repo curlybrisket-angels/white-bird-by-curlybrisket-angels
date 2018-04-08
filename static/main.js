9c03c5fda7c1368ce3e2b34c4d86363dbd2b68a1
Aidenjn <nelsonai@oregonstate.edu>
2018-04-08 09:52:35 -0700

    Commented out main.js code

--- static/main.js
+++ static/main.js
@@ -1,14 +1,16 @@
-var titleBlock = document.getElementById('titleBlock');
-var searchButton = document.getElementById('searchButton');
-var category1 = document.getElementById('formInputCategory1');
+//CODE UNDER DEVELOPMENT
 
-
-
-function submitClick() {
-    alert("yodel")
-    document.getElementById('searchResults').style.display = "block";
-    document.getElementById('searchResults').scrollIntoView();
-};
+//var titleBlock = document.getElementById('titleBlock');
+//var searchButton = document.getElementById('searchButton');
+//var category1 = document.getElementById('formInputCategory1');
+//
+//
+//
+//function submitClick() {
+//    alert("yodel")
+//    document.getElementById('searchResults').style.display = "block";
+//    document.getElementById('searchResults').scrollIntoView();
+//};
 
 
 //searchButton.addEventListener("click", setTimeout(submitClick, 500));
