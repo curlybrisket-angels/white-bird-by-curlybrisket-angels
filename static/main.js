80b4fae2e438aee2b8bcc5cf8fe7de8c8419ecd8
Aidenjn <nelsonai@oregonstate.edu>
2018-04-07 19:14:39 -0700

    Some final asthetic changes

--- static/main.js
+++ static/main.js
@@ -1,9 +1,6 @@
 var titleBlock = document.getElementById('titleBlock');
 var searchButton = document.getElementById('searchButton');
-
-
-
-//var category1 = document.getElementById('formInputCategory1');
+var category1 = document.getElementById('formInputCategory1');
 
 
 
@@ -14,12 +11,8 @@ function submitClick() {
 
 function changeColor() {
     //titleBlock.style.backgroundColor = "red";
-    //alert("hey");
-//    modalBackdrop.style.display = 'block';
-//    twitModal.style.display = 'block';
 };
 
 
-changeColor();
-
+//changeColor();
 //titleBlock.addEventListener('click', changeColor);
